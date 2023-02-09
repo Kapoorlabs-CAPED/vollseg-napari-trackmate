1df29911a7d38a808125846b6b1efa0bc6a19d68
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-02-09 23:03:59 +0100

    root

--- src/vollseg_napari_trackmate/_widget.py
+++ src/vollseg_napari_trackmate/_widget.py
@@ -1097,6 +1097,7 @@ def plugin_wrapper_track():
         plugin.track_model_type.value = value
         select_track_nature()
         plot_main()
+        show_track(None)
 
     @change_handler(
         plugin_color_parameters.spot_attributes,
