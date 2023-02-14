c8c862a65de6f3a583b165cef87450893239cc10
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-02-14 19:05:54 +0100

    pytorch file selection

--- src/vollseg_napari_trackmate/_widget.py
+++ src/vollseg_napari_trackmate/_widget.py
@@ -768,7 +768,7 @@ def plugin_wrapper_track():
         (
             cloud_auto_encoder_model_type,
             model_cloud_auto_encoder,
-        ) = *model_selected_cloud_auto_encoder
+        ) = model_selected_cloud_auto_encoder
         config = model_cloud_auto_encoder_configs[
             (cloud_auto_encoder_model_type, model_cloud_auto_encoder)
         ]
