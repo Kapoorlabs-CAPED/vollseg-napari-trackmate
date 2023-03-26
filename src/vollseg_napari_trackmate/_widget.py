2accd8b104bb74947264dd5d54e7dd6083ef834c
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-03-26 01:31:23 +0100

    stripplot

--- src/vollseg_napari_trackmate/_widget.py
+++ src/vollseg_napari_trackmate/_widget.py
@@ -1039,7 +1039,9 @@ def plugin_wrapper_track():
                         [global_data_cluster_plot, data_cluster_plot],
                         ignore_index=True,
                     )
-            global_data_cluster_plot["index"] = index_array
+            global_data_cluster_plot["index"] = index_array[
+                0 : len(global_data_cluster_plot)
+            ]
             global_data_cluster_plot = global_data_cluster_plot.set_index(
                 "index"
             )
