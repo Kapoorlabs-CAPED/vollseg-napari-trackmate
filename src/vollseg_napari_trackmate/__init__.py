5ca07c5e41816c34c8274ad4ed2f78794bf63e26
kapoorlab <randomaccessiblekapoor@gmail.com>
2023-10-07 19:17:36 +0200

    updates

--- src/vollseg_napari_trackmate/__init__.py
+++ src/vollseg_napari_trackmate/__init__.py
@@ -3,15 +3,13 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-from ._sample_data import make_sample_data
+from ._sample_data import get_test_tracks_xenopus
 from ._temporal_plots import TemporalStatistics
 from ._widget import plugin_wrapper_track
-from ._writer import write_multiple, write_single_image
 
 __all__ = (
-    "write_single_image",
     "write_multiple",
-    "make_sample_data",
+    "get_test_tracks_xenopus",
     "plugin_wrapper_track",
     "TemporalStatistics",
 )
